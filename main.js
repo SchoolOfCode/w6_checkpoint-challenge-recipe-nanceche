40df0e290da0af27d71b49c8f023199efc1a1f20
Your Name <you@example.com>
2021-05-30 15:05:56 +0200

    bad file removed

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let foodToSearch = null;
 let inputValue = null;
 const YOUR_APP_ID = "226b1ec4";
 const YOUR_APP_KEY = "a44fbda169b8fd8f4e12cc530240086f";
-let label = null;
+const resultsSection = document.getElementById("results");
+let data = null;
+let language = null;
 
 const requestUrl = `https://api.edamam.com/search?q=kale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
 
@@ -21,37 +23,67 @@ async function fetchRecipe() {
   const response = await fetch(
     `https://api.edamam.com/search?q=${foodToSearch}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
   );
-  let data = await response.json();
-  console.log(data);
-  let dataResult = data.hits[0];
-  console.log(dataResult);
-  label = dataResult.recipe.label;
-  console.log(label);
+  let { hits } = await response.json();
 
-  function changeLabel(label) {
-    let aTag = document.getElementById("recipe-label");
-    aTag.innerHTML = label;
-    console.log(label);
-    console.log(aTag.innerHTML);
+  //array of recipes
+  //print each recipe in array
+  //for loop as long as length
+  //for each item use display function
+  console.log(hits);
+  for (let i = 0; i < hits.length; i++) {
+    displayRecipeResults(hits[i].recipe);
   }
-  changeLabel(label);
 }
 
-// function changeLabel(label) {
-//   let aTag = document.getElementById("recipe-label");
-//   aTag.innerHTML = label;
-//   console.log(label);
-//   console.log(aTag.innerHTML);
+function displayRecipeResults(recipe) {
+  console.log(recipe);
+  let div = document.createElement("div");
+  div.setAttribute("class", "imageContainer");
+
+  //creating image tag and appending image to div
+  let image = document.createElement("img");
+  image.setAttribute("src", recipe.image);
+  div.appendChild(image);
+
+  //creating label, appending to div tag
+  let label = recipe.label;
+  let aTag = document.createElement("a");
+  aTag.innerHTML = label;
+  aTag.setAttribute("href", recipe.url);
+  div.appendChild(aTag);
+
+  //appending div to results section
+  resultsSection.appendChild(div);
+}
+
+// let ingredients=
+// async function getTranslation(element) {
+//   let response = await fetch(
+//     "https://google-translate1.p.rapidapi.com/language/translate/v2",
+//     {
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/x-www-form-urlencoded",
+//         "accept-encoding": "application/gzip",
+//         "x-rapidapi-key": "77199772cemsh7909a12b1b2c197p1a192cjsn8e11f888fc83",
+//         "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+//       },
+//       body: {
+//         q: "Hello, world!",
+//         target: "es",
+//         source: "en",
+//       },
+//     }
+//   );
+//   console.log(response);
+//   let data = await response.json();
+//   console.log(data);
 // }
-// changeLabel(label);
-//take in the input value
 
-//add value to the url
-
-// let inputValue2 = document.querySelector('[aria-label="food-input]');
-// function practice() {
-//   inputValue = document.querySelector("#food-input").value;
+// function handleTranslationInput() {
+//   languageToSearch = document.querySelector("#translation").value;
+//   return languageToSearch;
 // }
-
-// let button = document.querySelector("#recipe-button");
-// button.addEventListener("click", fetchRecipe);
+// function handleTranslationClick() {
+//   getTranslation(languageToSearch);
+// }
